9a0d03297df61ffbbf505c52fbcd815b521f4c02
Puxtril <puxtril@protonmail.com>
2023-07-05 20:34:12 -0400

    Added model format 99

--- include/model/ModelEnumMap.h
+++ include/model/ModelEnumMap.h
@@ -2,6 +2,7 @@
 
 #include "EnumMap.h"
 #include "model/types/ModelReader96.hpp"
+#include "model/types/ModelReader99.hpp"
 #include "model/types/ModelReader103.hpp"
 #include "model/types/ModelReader269.hpp"
 #include "model/types/ModelReader272.hpp"
@@ -10,6 +11,7 @@ namespace WarframeExporter::Model
 {
 	const static EnumMap<ModelReader> g_enumMapModel = EnumMap<ModelReader>()
 		.registerClass(ModelReader96::getInstance())
+		.registerClass(ModelReader99::getInstance())
 		.registerClass(ModelReader103::getInstance())
 		.registerClass(ModelReader269::getInstance())
 		.registerClass(ModelReader272::getInstance());

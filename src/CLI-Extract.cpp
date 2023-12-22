e4ce2cce26f7e668c029d5252e9540c973d24ae9
Puxtril <puxtril@protonmail.com>
2023-12-22 14:44:22 -0500

    More debug info

--- src/CLI-Extract.cpp
+++ src/CLI-Extract.cpp
@@ -82,6 +82,13 @@ CLIExtract::processCmd(const std::filesystem::path& outPath, const LotusLib::Lot
 		std::cin.get();
 	}
 
+	// Debug information
+	WarframeExporter::Logger::getInstance().debug("Type Flags: " + std::to_string(types));
+	std::stringstream pkgs;
+	for (int i = 0; i < pkgNames.size(); i++)
+		pkgs << (pkgNames[i]) << " ";
+	WarframeExporter::Logger::getInstance().debug("Loading packages: " + pkgs.str());
+
 	extract(cache, pkgNames, internalPath, outPath, (WarframeExporter::ExtractorType)types, ensmallening);
 }
 

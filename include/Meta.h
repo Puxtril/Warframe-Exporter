510252b8e49b5967b0103e09f12f775366ca5343
Puxtril <puxtril@protonmail.com>
2024-06-03 23:08:10 -0400

    Verison bump

--- include/Meta.h
+++ include/Meta.h
@@ -3,5 +3,5 @@
 #include <string_view>
 #include "spdlog/spdlog.h"
 
-const static std::string_view g_version = "2.6-LC1";
+const static std::string_view g_version = "2.6-LC2";
 const static spdlog::level::level_enum g_logLevel = spdlog::level::debug;

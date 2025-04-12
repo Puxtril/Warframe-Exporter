d410361712db94270d8b363d0fdd71f32b41c4b2
Puxtril <puxtril@protonmail.com>
2025-04-12 16:08:47 -0400

    Models export with pos/rot/scale applied at import
    I realized levelstatic models could have their size drastically reduced
    with deduplication, but this required pos/rot/scale to be applied on
    glTF nodes, not pre-applied to vertices. To make this a completed (good)
    implementation, changes will need to be made to how Models and Levels
    (non-static) are exported. But that will come later. Probably.
    Hopefully.

--- src/levelstatic/LevelStaticConverter.cpp
+++ src/levelstatic/LevelStaticConverter.cpp
@@ -7,20 +7,8 @@ WarframeExporter::LevelStatic::Converter::applyTransformation(LevelStaticObjectE
 	{
 		// Undo mirror by Model extractor
 		curVec.z *= -1.0f;
-
-		//curVec.y *= -1.0;
-
-		// Apply level object transforms
-		curVec *= glm::vec3(levelObj.scale, levelObj.scale, levelObj.scale);
-		curVec = glm::rotate(levelObj.rot, curVec);
-		
-		// Re-mirror
-		curVec.z *= -1.0f;
 	}
-
-	//levelObj.scale = 1.0f;
-	//levelObj.rot = glm::quat();
-
+	
 	// Reflect model origin point
 	// Apply this at export-time
 	levelObj.pos.z *= -1.0f;
--- src/levelstatic/LevelStaticExporterGltf.cpp
+++ src/levelstatic/LevelStaticExporterGltf.cpp
@@ -26,7 +26,18 @@ WarframeExporter::LevelStatic::ExporterGltf::_addLevelInformation(Document& gltf
         int32_t meshIndex = meshIndices[x];
 		Node& curNode = gltfDoc.nodes[meshIndex];
 
-        curNode.translation = std::array<float, 3>{levelObj.pos.x, levelObj.pos.y, levelObj.pos.z};
+        glm::mat4 matrix(1.0f);
+        matrix = glm::translate(matrix, {levelObj.pos.x / 2.0f, levelObj.pos.y / 2.0f, levelObj.pos.z / 2.0f});
+        matrix *= glm::scale(matrix, {1.0, 1.0, -1.0});
+        matrix *= glm::toMat4(levelObj.rot);
+        matrix = glm::scale(matrix, {levelObj.scale, levelObj.scale, levelObj.scale});
+
+        curNode.matrix = {
+            matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3],
+            matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3],
+            matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3],
+            matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3],
+        };
 
         Mesh& curMesh = gltfDoc.meshes[curNode.mesh];
 		curMesh.extensionsAndExtras["extras"]["Mesh"] = levelStaticHeaderExt.modelPaths[levelObj.modelIndex];

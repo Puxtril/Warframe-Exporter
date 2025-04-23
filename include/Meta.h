9e5c18654ade42effe36311d99bc24f78bbbf7d2
Puxtril <puxtril@protonmail.com>
2025-04-22 23:48:14 -0400

    Version switch

--- include/Meta.h
+++ include/Meta.h
@@ -3,5 +3,5 @@
 #include <string_view>
 #include "spdlog/spdlog.h"
 
-const static std::string_view g_version = "2.9-Alpha1";
+const static std::string_view g_version = "LevelStatic-1";
 const static spdlog::level::level_enum g_logLevel = spdlog::level::debug;

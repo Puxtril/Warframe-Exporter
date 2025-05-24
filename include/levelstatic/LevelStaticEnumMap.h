b714c92cc26ed19bbb5cb25d0f939b214fc7e4a3
Puxtril <puxtril@protonmail.com>
2025-05-24 16:27:50 -0400

    Added Levelstatic 29

--- include/levelstatic/LevelStaticEnumMap.h
+++ include/levelstatic/LevelStaticEnumMap.h
@@ -2,10 +2,12 @@
 
 #include "EnumMap.h"
 #include "levelstatic/types/LevelStaticReader28.h"
+#include "levelstatic/types/LevelStaticReader29.h"
 
 namespace WarframeExporter::LevelStatic
 {
 	const static EnumMap<LevelStaticReader> g_enumMapLevelStatic = EnumMap<LevelStaticReader>() 
-        .registerClass(LevelStaticReader28::getInstance()
+        .registerClass(LevelStaticReader28::getInstance())
+        .registerClass(LevelStaticReader29::getInstance()
     );
 }

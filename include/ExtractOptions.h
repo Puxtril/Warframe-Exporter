3f382bfd3854a8751bf4bd9b8f08a19a9cfa2ee2
Puxtril <puxtril@protonmail.com>
2025-08-06 22:17:23 -0400

    Make PNG the default texture export format

--- include/ExtractOptions.h
+++ include/ExtractOptions.h
@@ -11,7 +11,7 @@ namespace WarframeExporter
         bool dryRun = false;
         bool filterUiFiles = false;
         bool extractVertexColors = false;
-        WarframeExporter::Texture::TextureExportType textureExportType = WarframeExporter::Texture::TextureExportType::TEXTURE_EXPORT_DDS;
+        WarframeExporter::Texture::TextureExportType textureExportType = WarframeExporter::Texture::TextureExportType::TEXTURE_EXPORT_PNG;
         WarframeExporter::Shader::ShaderExportType shaderExportType = WarframeExporter::Shader::ShaderExportType::SHADER_EXPORT_BINARY;
         WarframeExporter::Level::LevelHlodExtractMode levelHlodExtractMode = WarframeExporter::Level::LevelHlodExtractMode::IGNORE;
     };

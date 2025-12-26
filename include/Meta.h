94421d09bb7fcda1e8092ca490097b23f3d694af
Puxtril <puxtril@protonmail.com>
2025-12-26 18:32:24 -0500

    Version bump 2.12

--- include/Meta.h
+++ include/Meta.h
@@ -3,5 +3,5 @@
 #include <string_view>
 #include "spdlog/spdlog.h"
 
-const static std::string_view g_version = "2.11";
+const static std::string_view g_version = "2.12";
 const static spdlog::level::level_enum g_logLevel = spdlog::level::debug;

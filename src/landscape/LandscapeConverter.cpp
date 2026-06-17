e6f055105bb861ae39190a9b41d6881c69aaf9e9
Puxtril <puxtril@protonmail.com>
2026-06-17 18:51:45 -0400

    Update landscape Y-axis scale

--- src/landscape/LandscapeConverter.cpp
+++ src/landscape/LandscapeConverter.cpp
@@ -47,11 +47,11 @@ LandscapeConverter::scaleChunks(Physx::HeightFieldIndexedMesh& mesh, const Lands
 
         // Bring into 0.0 - 1.0 range using Physx data
         curVert[0] = (curVert[0] - extBodyChunk.header.minBounds[0]) / (extBodyChunk.header.maxBounds[0] - extBodyChunk.header.minBounds[0]);
-        curVert[1] /= 256;
         curVert[2] = (curVert[2] - extBodyChunk.header.minBounds[2]) / (extBodyChunk.header.maxBounds[2] - extBodyChunk.header.minBounds[2]);
-
+        
         // Bring into Warframe's scale
         curVert[0] *= extHeaderChunk.scale.x;
+        curVert[1] = (curVert[1] * (extHeaderChunk.scale.y / 128.0) / 256.0);
         curVert[2] *= extHeaderChunk.scale.z;
     }
 }
